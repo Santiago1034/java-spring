cc7eef90070c92ad3a24695a586b8d9bd212a3d4
julian <julirojastoque@gmail.com>
2023-03-08 22:24:51 -0500

    mostrar

--- src/main/java/com/example/prueba/HolaMundo.java
+++ src/main/java/com/example/prueba/HolaMundo.java
@@ -10,29 +10,39 @@ import javax.swing.*;
 
 @RestController
 public class HolaMundo {
+    Scanner sc = new Scanner(System.in);
 
     @RequestMapping("/")
     public String home() {
-        return "<a href='suma'>suma<a><br>"+
+        return "<a href='suma'>suma<a><br>" +
                 "<a href='lista'>Arraylist<a>";
     }
 
     @RequestMapping("/suma")
-    public String suma(){
-        Scanner sc = new Scanner(System.in);
+    public String suma() {
         System.out.println("Digite un numero");
         int num1 = sc.nextInt();
         System.out.println("Digite un numero");
         int num2 = sc.nextInt();
         int ope = num1 + num2;
-        return "<a style='color:black' href='/'>Home<a>" + ope;
+        return "<a href='/'>Home<a><br>" + ope;
 
     }
 
 
     @RequestMapping("/lista")
-        public String valor(){
-        
-        }
+    public String valor() {
+        System.out.println("Ingrese su nombre");
+        String nombre = sc.next();
+        int edad = sc.nextInt();
+        int telefono = sc.nextInt();
+        int id = sc.nextInt();
+        int altura = sc.nextInt();
+        persona user = new persona(nombre, edad, telefono, id, altura);
+
+        return "<a href='/'>Home</a><br>" +
+                "<h2>Sus datos son: </h2><br>" +
+                user;
     }
+}
 
