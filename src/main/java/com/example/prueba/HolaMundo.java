4326197b97f55319ebd30fb2bcc5cc9cb074d8de
julian <julirojastoque@gmail.com>
2023-03-08 22:35:59 -0500

    importacion_objeto

--- src/main/java/com/example/prueba/HolaMundo.java
+++ src/main/java/com/example/prueba/HolaMundo.java
@@ -1,5 +1,6 @@
 package com.example.prueba;
 
+import com.example.prueba.model.persona;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
