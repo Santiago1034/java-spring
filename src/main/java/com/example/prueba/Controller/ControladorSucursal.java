150307aff7a8b3cf0d2794ab3080b82148c4817a
julian <julirojastoque@gmail.com>
2023-03-23 11:18:06 -0500

    metodos

--- src/main/java/com/example/prueba/Controller/ControladorSucursal.java
+++ src/main/java/com/example/prueba/Controller/ControladorSucursal.java
@@ -2,6 +2,9 @@ package com.example.prueba.Controller;
 
 import com.example.prueba.Entidades.Sucursal;
 import com.example.prueba.Servicios.ServicioSucursal;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -19,5 +22,20 @@ public class ControladorSucursal {
     @PostMapping("/insertar-sucursal")
     public String insertar(@RequestBody Sucursal s){
         return sc.agregarSucursal(s);
+    }
+
+    @GetMapping("/buscarSucursal/{datos}")
+    public ArrayList<Sucursal> buscar(@PathVariable("datos") String json){
+        return sc.buscarSucursal(json);
+    }
+
+    @GetMapping("/eliminarSucursal/{cod}")
+    public String eliminar(@PathVariable("cod") int codigo) {
+        return sc.eliminarSucursal(codigo);
+    }
+
+    @GetMapping("/actualizarSucursal/{Suc}")
+    public String actualizar(@PathVariable("Suc") String jsonuser){
+        return sc.actualizarSucursal(jsonuser);
     }
 }
